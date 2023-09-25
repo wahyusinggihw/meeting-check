2071308ac09ab5107106c8593ac4770b7b12a25c
wahyusinggihw <wahyusinggih2402@gmail.com>
2023-09-25 21:59:17 +0800

    update layout 2

--- app/Views/Dashboard/Layout/navbar.php
+++ app/Views/Dashboard/Layout/navbar.php
@@ -5,6 +5,9 @@
          <li class="nav-item">
              <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
          </li>
+         <li class="nav-item">
+             <a class="nav-link" href="<?= base_url('/') ?>" target="_blank" role="button"><i class="fas fa-link"></i></a>
+         </li>
          <!-- <li class="nav-item d-none d-sm-inline-block">
              <a href="#" class="nav-link">Home</a>
          </li>
--- app/Views/home.php
+++ app/Views/home.php
@@ -2,69 +2,103 @@
 
 <?= $this->section('content') ?>
 
-<!-- Small boxes (Stat box) -->
-<div class="row">
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-info">
-            <div class="inner">
-                <h3>150</h3>
-
-                <p>New Orders</p>
+<div class="col-sm-6 col-lg-3">
+    <div class="card">
+        <div class="card-body">
+            <div class="d-flex align-items-center">
+                <div class="subheader">Revenue</div>
+                <div class="ms-auto lh-1">
+                    <div class="dropdown">
+                        <a class="dropdown-toggle text-muted" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Last 7 days</a>
+                        <div class="dropdown-menu dropdown-menu-end">
+                            <a class="dropdown-item active" href="#">Last 7 days</a>
+                            <a class="dropdown-item" href="#">Last 30 days</a>
+                            <a class="dropdown-item" href="#">Last 3 months</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="icon">
-                <i class="ion ion-bag"></i>
+            <div class="d-flex align-items-baseline">
+                <div class="h1 mb-0 me-2">$4,300</div>
+                <div class="me-auto">
+                    <span class="text-green d-inline-flex align-items-center lh-1">
+                        8% <!-- Download SVG icon from http://tabler-icons.io/i/trending-up -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M3 17l6 -6l4 4l8 -8" />
+                            <path d="M14 7l7 0l0 7" />
+                        </svg>
+                    </span>
+                </div>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
+        <div id="chart-revenue-bg" class="chart-sm"></div>
     </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-success">
-            <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                <p>Bounce Rate</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-stats-bars"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-warning">
-            <div class="inner">
-                <h3>44</h3>
-
-                <p>User Registrations</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-person-add"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-danger">
-            <div class="inner">
-                <h3>65</h3>
-
-                <p>Unique Visitors</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <!-- ./col -->
 </div>
-<!-- /.row -->
+<div class="col-sm-6 col-lg-3">
+    <div class="card">
+        <div class="card-body">
+            <div class="d-flex align-items-center">
+                <div class="subheader">New clients</div>
+                <div class="ms-auto lh-1">
+                    <div class="dropdown">
+                        <a class="dropdown-toggle text-muted" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Last 7 days</a>
+                        <div class="dropdown-menu dropdown-menu-end">
+                            <a class="dropdown-item active" href="#">Last 7 days</a>
+                            <a class="dropdown-item" href="#">Last 30 days</a>
+                            <a class="dropdown-item" href="#">Last 3 months</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex align-items-baseline">
+                <div class="h1 mb-3 me-2">6,782</div>
+                <div class="me-auto">
+                    <span class="text-yellow d-inline-flex align-items-center lh-1">
+                        0% <!-- Download SVG icon from http://tabler-icons.io/i/minus -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M5 12l14 0" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+            <div id="chart-new-clients" class="chart-sm"></div>
+        </div>
+    </div>
+</div>
+<div class="col-sm-6 col-lg-3">
+    <div class="card">
+        <div class="card-body">
+            <div class="d-flex align-items-center">
+                <div class="subheader">Active users</div>
+                <div class="ms-auto lh-1">
+                    <div class="dropdown">
+                        <a class="dropdown-toggle text-muted" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Last 7 days</a>
+                        <div class="dropdown-menu dropdown-menu-end">
+                            <a class="dropdown-item active" href="#">Last 7 days</a>
+                            <a class="dropdown-item" href="#">Last 30 days</a>
+                            <a class="dropdown-item" href="#">Last 3 months</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex align-items-baseline">
+                <div class="h1 mb-3 me-2">2,986</div>
+                <div class="me-auto">
+                    <span class="text-green d-inline-flex align-items-center lh-1">
+                        4% <!-- Download SVG icon from http://tabler-icons.io/i/trending-up -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M3 17l6 -6l4 4l8 -8" />
+                            <path d="M14 7l7 0l0 7" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+            <div id="chart-active-users" class="chart-sm"></div>
+        </div>
+    </div>
+</div>
 
 <?= $this->endSection() ?>
