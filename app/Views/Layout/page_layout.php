85e761e777bdafe01b9f4ef18acb7e5b9fe2a777
Kurniia02 <kurniameiarta26@gmail.com>
2023-10-05 11:54:48 +0800

    Tambahan carousel

--- app/Views/Layout/page_layout.php
+++ app/Views/Layout/page_layout.php
@@ -9,7 +9,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-  <link rel="stylesheet" href="<?= base_url('assets/css/carousel.css') ?>">
+
   <link rel="stylesheet" href="<?= base_url('assets/css/peran.css') ?>">
 
 </head>
@@ -27,6 +27,7 @@
 
   <?= $this->include('layout/footer'); ?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <script src="<?= base_url('assets/js/sliders.js') ?>"></script>
 </body>
 
 </html>
--- app/Views/home.php
+++ app/Views/home.php
@@ -6,17 +6,43 @@
   <div class="info">
     <h1>Daftar Hadir Rapat <br> Pemkab Buleleng</h1>
     <p>Silahkan Masukan ID yang Telah Di Peroleh</p>
-    <form action="/submit-kode" method="post">
-      <input type="text" id="inputAlphanumeric" name="inputAlphanumeric" placeholder="abc-123">
+    <form action="">
+      <input type="text" class="search" name="text" placeholder="Masukan Id">
       <button>Submit</button>
     </form>
 
   </div>
-  <div class="image">
-    <img class="carousel-icon" src="<?= base_url('assets/img/carousel.png') ?>">
+  <div class="sliders">
+    <div class="slider">
+    <div class="image">
+      <img class="carousel-icon" src="<?= base_url('assets/img/carousel-logo.png') ?>" alt="gambar 1">
+    </div>
+    <div class="image">
+      <img class="carousel-icon" src="<?= base_url('assets/img/carousel-1.png') ?>" alt="gambar 2">
+    </div>
+    <div class="image">
+      <img class="carousel-icon" src="<?= base_url('assets/img/carousel-2.png') ?>" alt="gambar 3">
+    </div>
+    <div class="image">
+      <img class="carousel-icon" src="<?= base_url('assets/img/carousel-3.png') ?>" alt="gambar 4">
+    </div>
   </div>
+    <!-- buttoon prev and next -->
+  <div class="buttons">
+    <button id="prev"><</button>
+    <button id="next">></button>
+  </div>
+  <ul class="dots">
+    <li class="active"></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+  </div>
+
+
+
+
 </div>
-
-<script src="<?php echo base_url('assets/js/home.js'); ?>"></script>
-
 <?= $this->endSection() ?>
