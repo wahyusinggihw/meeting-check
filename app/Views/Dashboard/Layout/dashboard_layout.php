b785343c4bbdd0766b359964cb6036b9deacbc7e
Black07Jack <Black07Jack@users.noreply.github.com>
2023-10-10 10:52:04 +0800

    Update Bug Content

--- app/Views/Dashboard/Layout/dashboard_layout.php
+++ app/Views/Dashboard/Layout/dashboard_layout.php
@@ -40,7 +40,7 @@
 
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__shake" src="<?= base_url('assets/dist/img/AdminLTELogo.png') ?>" alt="AdminLTELogo" height="60" width="60">
+      <img class="animation__shake" src="<?= base_url('assets/img/1.1.png') ?>" alt="AdminLTELogo" height="100" width="100">
     </div>
 
     <?= $this->include('dashboard/Layout/navbar'); ?>
