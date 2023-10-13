d74ac54e828a6434055b5354630cd65572cdb5ef
Kurniia02 <kurniameiarta26@gmail.com>
2023-10-13 09:44:29 +0800

    perbaikan haalaman daftar hadir

--- app/Views/Dashboard/daftar_hadir.php
+++ app/Views/Dashboard/daftar_hadir.php
@@ -3,22 +3,35 @@
 <?= $this->section('content') ?>
 
 <body>
-    <div class="form-group mb-3">
-        <form action="/dashboard/daftar-hadir/cari" method="post">
-            <?= csrf_field() ?>
+    <!-- form -->
+    <form action="/dashboard/daftar-hadir/cari" method="post">
+        <?= csrf_field() ?>
+        <!-- <div class="container"> -->
             <label for="daftar_agenda" class="form-label">Pilih Agenda</label>
-            <select name="daftar_agenda" id="daftar_agenda" class="form-select">
-                <option value="">Pilih Agenda Rapat</option>
-                <?php foreach ($agenda_rapat as $i) : ?>
-                    <option value="<?= $i['id_agenda'] ?>"><?= $i['judul_rapat'] ?></option>
-                <?php endforeach; ?>
-            </select>
-            <button class="btn btn-primary" type="submit">Cari</button>
-        </form>
-    </div>
+            <div class="row align-items-start">
+                <div class="col">
+                    <select name="daftar_agenda" id="daftar_agenda" class="form-select">
+                        <option value="">Pilih Agenda Rapat</option>
+                        <?php foreach ($agenda_rapat as $i) : ?>
+                            <option value="<?= $i['id_agenda'] ?>"><?= $i['judul_rapat'] ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+                <div class="col">
+                    <button id="submit" class="btn btn-primary" type="submit">Cari</button>
+                </div>
+            </div>
+        <!-- </div> -->
+    </form>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        $('#daftar_agenda').on('change', function() {
+            $('#submit').prop('disabled', !$(this).val());
+        }).trigger('change');
+    </script>
 
     <?php if ($daftar_hadir != null) : ?>
-        <a href="#" download class="download-button">Download File</a>
+        <a href="#" download class="btn btn-primary my-2">Download File</a>
         <!-- foreach php -->
         <div class="table-container">
             <table class="participant-table">
