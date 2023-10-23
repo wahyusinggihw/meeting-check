4e9db6c3ebe308926efb555c9e2a211db9fe4dfc
Black07Jack <Black07Jack@users.noreply.github.com>
2023-10-23 10:07:58 +0800

    Edit Halaman PDF

--- app/Views/Dashboard/pdf_template.php
+++ app/Views/Dashboard/pdf_template.php
@@ -6,6 +6,30 @@
 <head>
     <title><?= $agendaRapat['agenda_rapat'] ?> </title>
     <style>
+        #header {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        #header img {
+            max-width: 150px;
+            /* Atur lebar maksimum logo */
+            margin-bottom: 10px;
+            /* Beri jarak antara logo dan alamat */
+        }
+
+        /* Gaya untuk alamat perusahaan */
+        #header p {
+            margin: 0;
+            /* Hapus margin bawaan dari elemen paragraf */
+        }
+
+        /* Gaya untuk tanggal */
+        #header #tanggal {
+            font-weight: bold;
+            /* Atur tebal font tanggal */
+        }
+
         body {
             margin: 20px;
         }
@@ -36,22 +60,34 @@
 
 
 <body>
-    <div class="header">
-        <div class="title">DETAIL RAPAT</div>
+    <div id="header">
+        <img src="<?= $logo ?>" />
+        <p>Jl. Pahlawan, Banjar Tegal, Kec. Buleleng, Kabupaten Buleleng, Bali 81117</p>
+        <p>Tanggal: <?php echo date('d M Y'); ?></p>
     </div>
 
-    <div class="agenda">
-        Agenda Rapat: <?= $agendaRapat['agenda_rapat'] ?>
-    </div>
+    <div id="content">
+        <div class="header">
+            <div class="title">DETAIL RAPAT</div>
+        </div>
 
-    <div class="description">
-        Deskripsi: <?= $agendaRapat['deskripsi'] ?>
-    </div>
+        <div class="agenda">
+            Agenda Rapat: <?= $agendaRapat['agenda_rapat'] ?>
+        </div>
 
-    <div class="link">
-        Link Rapat:
-        <div>
-            <img id="qr" width="100px" src="<?= $qrCode ?>" alt="<?= $agendaRapat['id_agenda'] ?>" class="">
+        <div class="description">
+            Deskripsi: <?= $agendaRapat['deskripsi'] ?>
+        </div>
+
+        <div class="ID Rapat">
+            ID: <?= $agendaRapat['kode_rapat'] ?>
+        </div>
+
+        <div class="link">
+            Link Rapat:
+            <div>
+                <img id="qr" width="100px" src="<?= $qrCode ?>" alt="<?= $agendaRapat['id_agenda'] ?>" class="">
+            </div>
         </div>
     </div>
 </body>
--- app/Views/Layout/navbar.php
+++ app/Views/Layout/navbar.php
@@ -1,13 +1,9 @@
 <header>
     <div class="container">
         <div class="logo">
-            <a href="/">
+            <a href="/auth/login">
                 <img src="<?php echo base_url('assets/img/logo.png'); ?>" alt="Logo" width="100">
             </a>
         </div>
-        <ul class="links">
-            <li><a href="/auth/login" class="btn btn-primary">Masuk</a>
-            </li>
-        </ul>
     </div>
 </header>
