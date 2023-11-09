847f57a06f0735df6f2a7458ba778ef67b4f06f9
Kurniia02 <kurniameiarta26@gmail.com>
2023-11-09 15:55:10 +0800

    tampilan form yang terbaru

--- app/Views/Layout/page_layout.php
+++ app/Views/Layout/page_layout.php
@@ -2,8 +2,9 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>Meeting Check <?php isset($title) ? print('- ' . $title) : '' ?></title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap">
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css'>
@@ -12,11 +13,8 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-  
+
   <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('assets/css/peran.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('assets/css/formabsensi.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('assets/css/berhasil.css') ?>">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
   <!-- bagian timeline -->
